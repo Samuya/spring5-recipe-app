5eb154ea73a2ce9ce8715e30ebfc743fb283b75b
Samuya <Samuya@samuya-workstation>
2018-12-23 18:24:29 +0530

    IT_and_Maven-Fialsafe

--- spring5-recipe-app/src/test/java/guru/springframework/Spring5RecipeAppApplicationTestsIT.java
+++ spring5-recipe-app/src/test/java/guru/springframework/Spring5RecipeAppApplicationTestsIT.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5RecipeAppApplicationTests {
+public class Spring5RecipeAppApplicationTestsIT {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
+
